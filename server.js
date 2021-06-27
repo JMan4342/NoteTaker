3637e464dc463922eaa33cf615959aafe9add82b
Joseph Manning <jgmanning3@yahoo.com>
2021-06-26 22:05:35 -0700

    Attempt to render new notes to db.json, no success

--- server.js
+++ server.js
@@ -1,29 +1,45 @@
+const fs = require("fs/promises");
+
 const express = require("express");
 const app = express();
 
 const PORT = 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/public/index.html");
-});
-app.get("/notes", function (req, res) {
-    res.sendFile(__dirname + "/public/notes.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-// app.get("/assets/js/index.js", function (req, res) {
-//     res.sendFile(__dirname + "/public/assets/js/index.js");
-// });
-// app.get("/assets/css/styles.css", function (req, res) {
-//     res.sendFile(__dirname + "/public/assets/css/styles.css");
-// });
+app.get("/notes", function (req, res) {
+  res.sendFile(__dirname + "/public/notes.html");
+});
 
 // GET request to /api/notes
+app.get("/api/notes", async function (req, res) {
+  try {
+    const data = await fs.readFile("./db/db.json", "utf8");
+    res.json(JSON.parse(data));
+  } catch (err) {
+    res.status(500).end("Server failed");
+  }
+});
 
-app.listen(PORT, () => 
-    console.log(`App listening at http://localhost:${PORT}`)
+// POST request to /api/notes
+app.post("/api/notes", async function (req, res) {
+  const notes = req.body;
+  try {
+    const data = await fs.writeFile("./db/db.json", "utf8");
+    res.json(JSON.parse(data));
+    data.push(notes);
+  } catch (err) {
+    res.status(500).end("Server failed");
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
 );
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
